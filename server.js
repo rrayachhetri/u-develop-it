c77d5cf3ff800dfea904fa21e03fa136df140bd0
Rabin Raya Chhetri <rrayachhetri@gmail.com>
2023-09-12 13:02:12 -0700

    connected to db and test queried the table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,8 +8,36 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({
+        message: "This is eWorld, your communication is being reserved!",
+        planet: 'Selixicaz'
+    });
+});
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'N3p@lGHR',
+        database: 'election'
+
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
+
+
+
+//catchall route
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
