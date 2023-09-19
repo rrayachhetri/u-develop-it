a2a6314096b3f4d0bc02d9b157fd8e71db740b48
Rabin Raya Chhetri <rrayachhetri@gmail.com>
2023-09-18 22:16:29 -0700

    used foreign key to reference data in parties table

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name AS party_name
+  FROM candidates
+  LEFT JOIN parties ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -55,7 +57,10 @@ app.get("/api/candidates", (req, res) => {
 
 // GET a single candidate
 app.get("/api/candidates/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name AS party_name
+               FROM candidates
+               LEFT JOIN parties ON candidates.party_id = parties.id 
+               WHERE candidates id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -100,7 +105,7 @@ app.post("/api/candidate", ({ body }, res) => {
     "last_name",
     "industry_connected"
   );
-  if(errors) {
+  if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
