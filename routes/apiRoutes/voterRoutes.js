50ed9e66b1297ebed3d26b5db6358e686cfe951a
Rabin Raya Chhetri <rrayachhetri@gmail.com>
2023-09-29 00:35:29 -0700

    GET voters apiRoute

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+
+// get all voters
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// get a single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//Delete a voter
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.status(400).json({ error: err.message })
+            return;
+        } else if(!result.affectedRows){
+            res.json({
+                message: 'Voter not found'
+            });
+        } else {
+            res.json({
+                message: 'voter deleted successfully',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
+const express = require("express");
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-const mysql = require("mysql2");
-const express = require("express");
-const { route } = require("express/lib/application");
-const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// use apiRoutes
 app.use('./api', apiRoutes);
 
 app.get("/", (req, res) => {
@@ -21,69 +19,12 @@ app.get("/", (req, res) => {
   });
 });
 
-
-
-// GET all parties
-app.get('/api/parties', (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.query(sql, (err, rows) => {
-    if(err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-
-// get a single party
-app.get('/api/party/:id', (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, row) => {
-    if(err){
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: row
-    });
-  });
-});
-
-// Delete a party
-app.delete('/api/party/:id', (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
-    if(err){
-      res.status(400).json({ error: err.message });
-      // Checks if anything was deleted.
-    } else if(!result.affectedRows) {
-      res.json({
-        message: 'Party not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted successfully!',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
-  });
-});
-
-
-//catchall route
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// Start the server after DB connection
 db.connect(err => {
   if(err) throw err;
   console.log('Database connected.')
@@ -91,4 +32,6 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+
 
