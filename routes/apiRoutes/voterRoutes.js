c53583d71ac3bf149797f60f481b85a27b3066be
Rabin Raya Chhetri <rrayachhetri@gmail.com>
2023-10-15 23:43:46 -0700

    added voters routes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -38,11 +38,29 @@ router.get('/voter/:id', (req, res) => {
 
 //create a voter
 router.post('/voter', ({ body }, res) => {
+    // Data Validation
     const errors = inputCheck(body, 'first_name', 'last_name', 'email') ;
     if(errors) {
-        res.status(400).json({ error: errors });
-        return;   }
-})
+        res.status(400).json({ errors: errors });
+        return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES(?, ?,)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;   
+        }
+        res.json({
+            message: "success",
+            data: body,
+            changes: result.affectedRows
+        });
+
+    });
+  
+});
 
 
 //Delete a voter
@@ -66,5 +84,32 @@ router.delete('/voter/:id', (req, res) => {
         }
     });
 });
+
+
+//Update a voter info
+router.put('/voter/:id', (req, res) => {
+    //Data Validation
+    const errors = inputCheck(req.body, 'email');
+    if(errors) {
+        res.status(400).json({ errors: errors });
+        return;
+    }
+
+    const sql = `UPDATE voters SET email=? WHERE id=?`;
+    const params = [req.body.email, req.params.id];
+db.query(sql, params, (err, result) => {
+    if(err){
+        res.status(400).json({ err: err.message});
+        return;
+    }
+    res.json({
+        message: "voter email updated!",
+        data: req.body,
+        changes: result.affectedRows
+    });
+});
+
+});
+
 
 module.exports = router;
