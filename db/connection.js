7a4f6312c6888e310d29392ab98cfe94309d213b
Rabin <rrayachhetri@gmail.com>
2023-11-06 16:16:29 -0800

    --flag--

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     // Your mySQL username
-    user: 'root',
-    password: 'N3p@lGHR',
-    database: 'election'
+    user: '',
+    password: '',
+    database: ''
 });
 console.log('db connection successful!');
 
